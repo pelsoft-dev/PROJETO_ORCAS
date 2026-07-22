7cce2028b1a5a9279242f4269b4b334fabb9ff8e
PPelegrina <ppelegrina2025@gmail.com>
2026-07-21 23:27:27 -0300

    2026-07-21 23:19 Numeração dos Campos

--- orcas_v01_ajuda_gestao.py
+++ orcas_v01_ajuda_gestao.py
@@ -10,9 +10,9 @@ def renderizar_ajuda_gestao():
         <div style="background-color: #007ba7; padding: 15px; border-radius: 5px; color: white; font-family: sans-serif; margin-bottom: 20px; position: relative;">
             <div style="font-size: 14px; text-align: justify; line-height: 1.4;">
                 Se for sua primeira vez aqui no ORCAS, siga esse caminho:<br>
-                (1) Digite um nome para seu Plano no campo 02 e dê ENTER.<br>
-                (2) Se você quiser colocar um Saldo Inicial, utilize o campo 06.<br>
-                (3) Se você quiser receber um Resumo Diário via Whatsapp e/ou E-mail, utilize os campos 08 e 09.<br>
+                <p>(1) Digite um nome para seu Plano no campo 02 e dê ENTER.</p><br>
+                <p>(2) Se você quiser colocar um Saldo Inicial, utilize o campo 06.</p><br>
+                <p>(3) Se você quiser receber um Resumo Diário via Whatsapp e/ou E-mail, utilize os campos 08 e 09.<br>
                 (4) Escolha o tipo de Assinatura (Mensal, Semestral, Anual).<br>
                 (5) Clique no campo 12. Salvar alterações ou Criar o novo Plano. <br>
                 (6) 
