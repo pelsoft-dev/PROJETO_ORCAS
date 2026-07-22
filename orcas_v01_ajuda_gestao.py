4ccf77f33d93f3a26936bcf350577408dc3ea60e
PPelegrina <ppelegrina2025@gmail.com>
2026-07-21 23:59:01 -0300

    2026-07-21 23:58 Numeração dos Campos

--- orcas_v01_ajuda_gestao.py
+++ orcas_v01_ajuda_gestao.py
@@ -11,14 +11,14 @@ def renderizar_ajuda_gestao():
             <div style="font-size: 14px; text-align: justify; line-height: 1.4;">
                 <b>Se for sua 1ª vez ou quiser criar um outro Plano no ORCAS, siga esse caminho:</b><br>
                 (1) Digite um nome para seu Plano no campo 02 e dê ENTER.<br>
-                (2) Se você quiser colocar um Saldo Inicial, utilize o campo 06.<br>
-                (3) Se você quiser receber um Resumo Diário via Whatsapp e/ou E-mail, utilize os campos 08 e 09.<br>
-                (4) Escolha o tipo de Assinatura (Mensal, Semestral, Anual).<br>
-                (5) Clique no campo 12. Salvar alterações ou Criar o novo Plano. <br>
-                (6) Se tiver Cupom de Desconto, digite-o no campo 21 e dê ENTER.<br>
-                (7) Com ou sem Cupom, agora clique no botão "22. GERAR LINK DE PAGAMENTO".<br>
-                (8) Clique no botão "30. PAGAMENTO - IR P/ MERCADO PAGO".<br>
-                <b>Se quiser apenas carregar um Plano já existente, utilize o campo 01.</b><br>
+                (2) Se você quiser receber um Resumo Diário via Whatsapp e/ou E-mail, utilize os campos 08 e 09.<br>
+                (3) Escolha o tipo de Assinatura (Mensal, Semestral, Anual).<br>
+                (4) Clique no campo 12. Salvar alterações ou Criar o novo Plano. <br>
+                (5) Se tiver Cupom de Desconto, digite-o no campo 21 e dê ENTER.<br>
+                (6) Com ou sem Cupom, agora clique no botão "22. GERAR LINK DE PAGAMENTO".<br>
+                (7) Clique no botão "30. PAGAMENTO - IR P/ MERCADO PAGO".<br>
+                <b>Se quiser apenas carregar um Plano já existente, clique sobre o campo 01 e escolha.</b><br>
+                A qualquer momento você pode mudar a configuração de seu plano (saldo inicial, número de meses, resumo diário, etc)
             </div>
         </div>
         """, 
