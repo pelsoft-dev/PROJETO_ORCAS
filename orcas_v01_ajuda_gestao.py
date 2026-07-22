1e0049f2fa40e6c5bd13066f9b3fd19d682ace72
PPelegrina <ppelegrina2025@gmail.com>
2026-07-22 00:01:08 -0300

    2026-07-21 23:58 Numeração dos Campos

--- orcas_v01_ajuda_gestao.py
+++ orcas_v01_ajuda_gestao.py
@@ -10,15 +10,15 @@ def renderizar_ajuda_gestao():
         <div style="background-color: #007ba7; padding: 15px; border-radius: 5px; color: white; font-family: sans-serif; margin-bottom: 20px; position: relative;">
             <div style="font-size: 14px; text-align: justify; line-height: 1.4;">
                 <b>Se for sua 1ª vez ou quiser criar um outro Plano no ORCAS, siga esse caminho:</b><br>
-                (1) Digite um nome para seu Plano no campo 02 e dê ENTER.<br>
-                (2) Se você quiser receber um Resumo Diário via Whatsapp e/ou E-mail, utilize os campos 08 e 09.<br>
-                (3) Escolha o tipo de Assinatura (Mensal, Semestral, Anual).<br>
-                (4) Clique no campo 12. Salvar alterações ou Criar o novo Plano. <br>
-                (5) Se tiver Cupom de Desconto, digite-o no campo 21 e dê ENTER.<br>
-                (6) Com ou sem Cupom, agora clique no botão "22. GERAR LINK DE PAGAMENTO".<br>
-                (7) Clique no botão "30. PAGAMENTO - IR P/ MERCADO PAGO".<br>
+                   (1) Digite um nome para seu Plano no campo 02 e dê ENTER.<br>
+                   (2) Se você quiser receber um Resumo Diário via Whatsapp e/ou E-mail, utilize os campos 08 e 09.<br>
+                   (3) Escolha o tipo de Assinatura (Mensal, Semestral, Anual).<br>
+                   (4) Clique no campo 12. Salvar alterações ou Criar o novo Plano. <br>
+                   (5) Se tiver Cupom de Desconto, digite-o no campo 21 e dê ENTER.<br>
+                   (6) Com ou sem Cupom, agora clique no botão "22. GERAR LINK DE PAGAMENTO".<br>
+                   (7) Clique no botão "30. PAGAMENTO - IR P/ MERCADO PAGO".<br>
                 <b>Se quiser apenas carregar um Plano já existente, clique sobre o campo 01 e escolha.</b><br>
-                A qualquer momento você pode mudar a configuração de seu plano (saldo inicial, número de meses, resumo diário, etc)
+                   A qualquer momento você pode mudar a configuração de seu plano (saldo inicial, número de meses, resumo diário, etc)
             </div>
         </div>
         """, 
