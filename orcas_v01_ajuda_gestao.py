457809ed061427dc3f3d94e320aa7d57dbb192aa
PPelegrina <ppelegrina2025@gmail.com>
2026-09-07 22:36:41 -0300

    2026-09-07 22:16 ajuda em pdf

--- orcas_v01_ajuda_gestao.py
+++ orcas_v01_ajuda_gestao.py
@@ -5,21 +5,19 @@ import streamlit as st
 
 def renderizar_ajuda_gestao():
     """Renderiza o PDF de ajuda da tela de Gestão dentro de um container com rolagem."""
-    # caminho_pdf = "ajuda_gestao.pdf"  # Nome/caminho do seu PDF na pasta do projeto
-    caminho_pdf = "orcas-ajuda-pdf.pdf"  # Nome/caminho do seu PDF na pasta do projeto
+    caminho_pdf = "orcas-ajuda-pdf.pdf"
 
     if os.path.exists(caminho_pdf):
         with open(caminho_pdf, "rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
-        # Exibe o PDF em um iframe responsivo
+        # Utiliza o leitor da Mozilla via CDN para renderizar Data URI
         pdf_display = f"""
             <div style="background-color: #007ba7; padding: 10px; border-radius: 8px; margin-bottom: 20px;">
                 <iframe 
-                    src="data:application/pdf;base64,{base64_pdf}" 
+                    src="https://mozilla.github.io/pdf.js/web/viewer.html?file=data:application/pdf;base64,{base64_pdf}" 
                     width="100%" 
                     height="600px" 
-                    type="application/pdf"
                     style="border: none; border-radius: 5px;">
                 </iframe>
             </div>
